52ea83b980bde63baeabaf7410efdb4569ae3ec9
15179793794 <44147490+15179793794@users.noreply.github.com>
2020-12-25 19:09:47 -0600

    Add files via upload
    首次上传

---
+++ src/main/java/controller/LoginIn.java
@@ -0,0 +1,71 @@
+package controller;
+
+import com.google.code.kaptcha.Producer;
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.google.code.kaptcha.servlet.KaptchaServlet;
+import com.google.code.kaptcha.util.Config;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.Properties;
+
+/**
+ * @author 吴仁杨
+ */
+@WebServlet("/in")
+public class LoginIn extends HttpServlet {
+
+    private Properties props = new Properties();
+    private Producer kaptchaProducer = null;
+    private String sessionKeyValue = null;
+    private String sessionKeyDateValue = null;
+//    注册登录
+
+    public LoginIn() {
+    }
+
+    @Override
+    public void init(ServletConfig conf) throws ServletException {
+        super.init(conf);
+        ImageIO.setUseCache(false);
+        Enumeration initParams = conf.getInitParameterNames();
+
+        while(initParams.hasMoreElements()) {
+            String key = (String)initParams.nextElement();
+            String value = conf.getInitParameter(key);
+            this.props.put(key, value);
+        }
+
+        Config config = new Config(this.props);
+        this.kaptchaProducer = config.getProducerImpl();
+        this.sessionKeyValue = config.getSessionKey();
+        this.sessionKeyDateValue = config.getSessionDate();
+    }
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setDateHeader("Expires", 0L);
+        resp.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
+        resp.addHeader("Cache-Control", "post-check=0, pre-check=0");
+        resp.setHeader("Pragma", "no-cache");
+        resp.setContentType("image/jpeg");
+        String capText = this.kaptchaProducer.createText();
+        req.getSession().setAttribute(this.sessionKeyValue, capText);
+        req.getSession().setAttribute(this.sessionKeyDateValue, new Date());
+        BufferedImage bi = this.kaptchaProducer.createImage(capText);
+        System.out.println(capText);
+        ServletOutputStream out = resp.getOutputStream();
+        ImageIO.write(bi, "jpg", out);
+    }
+}
